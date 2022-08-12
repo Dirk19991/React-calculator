c99ef3885863f17deca7901d43398cce8a7e77ef
Dmitry Khodyrev <dooommm@mail.ru>
2022-08-12 14:56:01 +0800

    Updates

---
+++ precache-manifest.46e99692f60f545c71be2d171d6850bf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d71ebe4ed0d75777ec51",
+    "url": "/static/css/main.499d37c0.chunk.css"
+  },
+  {
+    "revision": "d71ebe4ed0d75777ec51",
+    "url": "/static/js/main.d71ebe4e.chunk.js"
+  },
+  {
+    "revision": "2fcd6d68bab7babc0e27",
+    "url": "/static/js/1.2fcd6d68.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "38e10a9a02454bad2f97499cc330b9af",
+    "url": "/index.html"
+  }
+];
